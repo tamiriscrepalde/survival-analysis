083ab3664b515c61adfcc231c775f5d2e751e230
Tamiris Crepalde <tamiriscrepalde@poli.ufrj.br>
2023-01-29 17:15:09 -0300

    Update remote

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 """Useful general functions."""
 
+import pandas as pd
+
 
 def read_file(file_path: str) -> str:
     """Read file and returns its content.
@@ -15,3 +17,19 @@ def read_file(file_path: str) -> str:
     file.close()
 
     return content
+
+
+def convert_to_boolean(df: pd.DataFrame, col: str) -> pd.DataFrame:
+    """Converts a column to boolean.
+
+    Args:
+        df (pd.DataFrame): DataFrame containing column of interest.
+        col (str): Column to be converted.
+
+    Returns:
+        pd.DataFrame: DataFrame containing converted column.
+    """
+    df.loc[df[col].isna(), col] = False
+    df.loc[~df[col].isna(), col] = True
+
+    return df
