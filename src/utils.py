7ee1da561f74727371878cfc0952b135343583c6
Tamiris Crepalde <tamiriscrepalde@poli.ufrj.br>
2023-02-10 18:48:12 -0300

    Update remote

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 """Useful general functions."""
 
+from typing import List
+
 import pandas as pd
 
 
@@ -19,17 +21,35 @@ def read_file(file_path: str) -> str:
     return content
 
 
-def convert_to_boolean(df: pd.DataFrame, col: str) -> pd.DataFrame:
+def convert_to_boolean(df: pd.DataFrame, column: str) -> pd.DataFrame:
     """Converts a column to boolean.
 
     Args:
         df (pd.DataFrame): DataFrame containing column of interest.
-        col (str): Column to be converted.
+        column (str): Column to be converted.
 
     Returns:
         pd.DataFrame: DataFrame containing converted column.
     """
-    df.loc[df[col].isna(), col] = False
-    df.loc[~df[col].isna(), col] = True
+    df.loc[df[column].isna(), column] = False
+    df.loc[~df[column].isna(), column] = True
 
     return df
+
+
+def create_date_columns(df: pd.DataFrame, columns: List[str]) -> pd.DataFrame:
+    """_summary_
+
+    Args:
+        df (pd.DataFrame): _description_
+        column (str): _description_
+
+    Returns:
+        pd.DataFrame: _description_
+    """
+    df_dates = pd.DataFrame()
+    for column in columns:
+        df_dates[f'{column}_year'] = df[column].dt.year
+        df_dates[f'{column}_month'] = df[column].dt.month
+
+    return df_dates
