a900505ef9c05a639d3550bb0d8bec1feec1eb8b
Tamiris Crepalde <tamiriscrepalde@poli.ufrj.br>
2023-02-15 11:08:18 -0300

    Update remote

--- src/featuring/__init__.py
+++
@@ -1,5 +0,0 @@
-from src.featuring.featuring_columns_utils import (
-    convert_to_datetime,
-    convert_to_int,
-    treat_accom_type,
-)
--- src/visualization/visualization_utils.py
+++ src/visualization/visualization_utils.py
@@ -83,7 +83,7 @@ def date_relationship(df: pd.DataFrame, column_min: str, column_max: str, new_co
         plt.tight_layout()
 
 
-def single_histplot(df: pd.DataFrame, column: str) -> None:
+def single_histplot(df: pd.DataFrame, column: str, **kwargs) -> None:
     """Plot a single histplot.
 
     Args:
@@ -92,40 +92,7 @@ def single_histplot(df: pd.DataFrame, column: str) -> None:
     """
     plt.figure(figsize=(15, 8))
 
-    sns.histplot(df[column], stat='proportion')
+    sns.histplot(df, x=column, stat='proportion', **kwargs)
 
     plt.title(f'Proportion of {column} for order.')
     plt.xticks(df[column].unique())
-
-
-
-
-
-
-
-
-
-# def vis_corr_matrix(df: pd.DataFrame) -> None:
-#     """
-#     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ullamcorper leo ut dignissim
-#     vulputate.
-#     Args:
-#         param1 (str): Description of `param1`.
-#     Returns:
-#         Lorem ipsum dolor sit amet
-#     """
-#     corr = df.corr()
-#     mask = np.zeros_like(corr, dtype=np.bool)
-#     mask[np.triu_indices_from(mask)] = True
-#     plt.figure(figsize=(13, 13))
-#     cmap = sns.diverging_palette(220, 10, as_cmap=True)
-#     with sns.axes_style("white"):
-#         sns.heatmap(
-#             corr,
-#             cmap=cmap,
-#             center=0,
-#             mask=mask,
-#             square=True,
-#             linewidths=1.5,
-#             cbar_kws={"shrink": 0.5},
-#         )
